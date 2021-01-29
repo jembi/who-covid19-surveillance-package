ad2288345e3667a02b56a60d88036a859d381112
MattyJ007 <matthew.dickie@jembi.org>
2021-01-29 10:28:56 +0200

    Copy over basic package import logic
    This logic will spin up a mapping mediator instance and add scripts for
    importing an endpoint template - import script is not working just yet - requires
    manual request to get endpoint inported
    
    DSC19-25

---
+++ docker/importer/volume/endpoint.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const fs = require('fs')
+const http = require('http')
+const path = require('path')
+
+const MEDIATOR_HOSTNAME = process.env.MEDIATOR_HOST_NAME || 'covid19-surveillance-mediator'
+const MEDIATOR_API_PORT = process.env.MEDIATOR_API_PORT || 3003
+
+const jsonData = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, 'endpoint.json'))
+)
+
+const data = JSON.stringify(jsonData)
+
+const options = {
+  protocol: 'http:',
+  host: MEDIATOR_HOSTNAME,
+  port: MEDIATOR_API_PORT,
+  path: '/endpoints',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+const req = http.request(options, res => {
+  if (res.statusCode === 400) {
+    let data = ''
+    res.on('data', chunk => {
+      data += chunk.toString()
+    })
+
+    res.on('end', () => {
+      if (data) {
+        data = JSON.parse(data)
+        if (
+          data.error && data.error.match(/duplicate key error/).length
+          ) {
+          console.log('covid19-surveillance endpoint already exists')
+          return
+        }
+        throw Error('covid19-surveillance endpoint creation failed')
+      }
+    })
+
+    res.on('error', err => {
+      throw Error(err)
+    })
+    return
+  }
+
+  if (res.statusCode != 201) {
+    throw Error(`Failed to create covid19-surveillance mediator endpoint: ${res.statusCode}`)
+  } else {
+    console.log('Successfully created covid19-surveillance endpoint')
+  }
+})
+
+req.on('error', error => {
+  console.error('Failed to create covid19-surveillance mediator endpoint: ', error)
+})
+
+req.write(data)
+req.end()
