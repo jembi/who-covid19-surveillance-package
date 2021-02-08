2e56ed0160ff150c53f64130fa64456e56d82fb4
MattyJ007 <matthew.dickie@jembi.org>
2021-02-08 17:22:23 +0200

    Update endpoint importer
    WIP: Get the package to auto import data
    
    DSC19-25

--- docker/importer/volume/endpoint.js
+++ docker/importer/volume/endpoint.js
@@ -4,62 +4,75 @@ const fs = require('fs')
 const http = require('http')
 const path = require('path')
 
-const MEDIATOR_HOSTNAME = process.env.MEDIATOR_HOST_NAME || 'covid19-surveillance-mediator'
+const MEDIATOR_HOSTNAME =
+  process.env.MEDIATOR_HOST_NAME || 'covid19-surveillance-mediator'
 const MEDIATOR_API_PORT = process.env.MEDIATOR_API_PORT || 3003
 
-const jsonData = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, 'endpoint.json'))
-)
+const ENDPOINT_FILES = [
+  'endpoint-handler.json',
+  'endpoint-flattener.json',
+  'endpoint-resource-resolver.json',
+  'endpoint-location.json',
+  'endpoint-bundler.json',
+  'endpoint-composition.json',
+  'endpoint-encounter.json',
+  'endpoint-observation.json'
+]
 
-const data = JSON.stringify(jsonData)
+ENDPOINT_FILES.forEach(file => {
+  const data = fs.readFileSync(path.resolve(__dirname, file))
 
-const options = {
-  protocol: 'http:',
-  host: MEDIATOR_HOSTNAME,
-  port: MEDIATOR_API_PORT,
-  path: '/endpoints',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
+  const options = {
+    protocol: 'http:',
+    host: MEDIATOR_HOSTNAME,
+    port: MEDIATOR_API_PORT,
+    path: '/endpoints',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }
-}
 
-const req = http.request(options, res => {
-  if (res.statusCode === 400) {
-    let data = ''
-    res.on('data', chunk => {
-      data += chunk.toString()
-    })
+  const req = http.request(options, (res) => {
+    if (res.statusCode === 400) {
+      let data = ''
+      res.on('data', (chunk) => {
+        data += chunk.toString()
+      })
 
-    res.on('end', () => {
-      if (data) {
-        data = JSON.parse(data)
-        if (
-          data.error && data.error.match(/duplicate key error/).length
-          ) {
-          console.log('covid19-surveillance endpoint already exists')
-          return
+      res.on('end', () => {
+        if (data) {
+          data = JSON.parse(data)
+          if (data.error && data.error.match(/duplicate key error/).length) {
+            console.log('covid19-surveillance endpoint already exists')
+            return
+          }
+          throw Error('covid19-surveillance endpoint creation failed')
         }
-        throw Error('covid19-surveillance endpoint creation failed')
-      }
-    })
+      })
 
-    res.on('error', err => {
-      throw Error(err)
-    })
-    return
-  }
+      res.on('error', (err) => {
+        throw Error(err)
+      })
+      return
+    }
 
-  if (res.statusCode != 201) {
-    throw Error(`Failed to create covid19-surveillance mediator endpoint: ${res.statusCode}`)
-  } else {
-    console.log('Successfully created covid19-surveillance endpoint')
-  }
+    if (res.statusCode != 201) {
+      throw Error(
+        `Failed to create covid19-surveillance mediator endpoint: ${res.statusCode}`
+      )
+    } else {
+      console.log('Successfully created covid19-surveillance endpoint')
+    }
+  })
+
+  req.on('error', (error) => {
+    console.error(
+      'Failed to create covid19-surveillance mediator endpoint: ',
+      error
+    )
+  })
+
+  req.write(data)
+  req.end()
 })
-
-req.on('error', error => {
-  console.error('Failed to create covid19-surveillance mediator endpoint: ', error)
-})
-
-req.write(data)
-req.end()
