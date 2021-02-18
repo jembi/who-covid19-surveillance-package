725d05a5128d0841094fa5cea7088f6848add35b
Lazola Sifuba <sifubalazola@gmail.com>
2021-02-18 17:56:01 +0200

    Add Docker and k8s import scripts
    This commit adds import scripts for both Docker and K8s. The functionality
    for the script is not yet fully working as there is a
    error from HAPI FHIR itself.
    
    Related Tickets:
     - DSC19-21
     - DSC19-43

---
+++ kubernetes/importer/volume/fhirConfig.js
@@ -0,0 +1,102 @@
+'use strict'
+
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
+const path = require('path')
+const { spawn } = require('child_process')
+
+const resourcesPath = path.resolve(__dirname, 'fhirResources')
+
+const downloadResources = (cbk) => {
+  const COVID19_IG_URL = process.env.COVID19_IG_URL || 'https://openhie.github.io/covid-ig'
+  fs.mkdirSync(resourcesPath)
+  
+  const resources = fs.createWriteStream(path.resolve(resourcesPath, 'fhir-resources.zip'))
+
+  https.get(`${COVID19_IG_URL}/definitions.json.zip`, (response) => {
+    response.pipe(resources)
+    resources.on('finish', () => {
+      resources.close()
+
+      const unzip = spawn('unzip', [path.resolve(resourcesPath, 'fhir-resources.zip'), '-d', resourcesPath])
+      unzip.stderr.on('data', cbk)
+
+      unzip.on('close', () => {
+        const allFiles = fs.readdirSync(resourcesPath) || []
+        return cbk(null, allFiles.filter(file => file.endsWith('.json')))
+      })
+    })
+  }).on('error', cbk)
+}
+
+const postToFHIRServer = (files) => {
+  const HAPI_FHIR_HOSTNAME =
+    process.env.HAPI_FHIR_HOSTNAME || 'hapi-fhir'
+  const HAPI_FHIR_PORT = process.env.HAPI_FHIR_PORT || 3447
+
+  files.forEach(file => {
+    const resourceName = file.split(/\-/)[0]
+    const data = fs.readFileSync(path.resolve(resourcesPath, file))
+  
+    const options = {
+      protocol: process.env.COVID19_IG_PROTOCOL || 'http:',
+      host: HAPI_FHIR_HOSTNAME,
+      port: HAPI_FHIR_PORT,
+      path: `hapi-fhir-jpaserver/fhir/${resourceName}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      }
+    }
+  
+    const req = http.request(options, (res) => {
+      if (res.statusCode === 400) {
+        let data = ''
+        res.on('data', (chunk) => {
+          data += chunk.toString()
+        })
+  
+        res.on('end', () => {
+          if (data) {
+            data = JSON.parse(data)
+            if (data.error) {
+              console.log(data.error)
+              return
+            }
+            throw Error(`${resourceName} resource creation failed`)
+          }
+        })
+  
+        res.on('error', (err) => {
+          throw Error(err)
+        })
+        return
+      }
+  
+      if (res.statusCode != 201) {
+        throw Error(
+          `Failed to create ${resourceName} resource: ${res.statusCode}`
+        )
+      } else {
+        console.log(`Successfully created ${resourceName} resource`)
+      }
+    })
+  
+    req.on('error', (error) => {
+      console.error('Failed to add resource: ', error)
+    })
+  
+    req.write(data)
+    req.end()
+  })
+}
+
+downloadResources((err, files) => {
+  if (err) {
+    console.error(err)
+  } else {
+    postToFHIRServer(files)
+  }
+})
