fe378c85edf5b29faa8f27099eb1616faf77f657
Lazola Sifuba <sifubalazola@gmail.com>
2021-02-26 12:35:16 +0200

    Post resources concurrently to the FHIR server
    This commit improves the functionality for posting to HAPI FHIR. Previously,
    the resources were processed and posted one after the other using the http.request
    function. This functionality seems to have introduced race-conditions HAPI FHIR
    as multiple errors were reported for duplicate ids for resources.
    
    This commit has addressed the duplicate ids issue by wrapping
    each POST to the FHIR server in a promise.
    
    Related Tickets:
     - DSC19-21

--- docker/importer/volume/fhirConfig.js
+++ docker/importer/volume/fhirConfig.js
@@ -7,6 +7,9 @@ const path = require('path')
 const { spawn } = require('child_process')
 
 const resourcesPath = path.resolve(__dirname, 'fhirResources')
+const HAPI_FHIR_PATH = process.env.HAPI_FHIR_PATH || '/fhir'
+const HAPI_FHIR_HOSTNAME = process.env.HAPI_FHIR_HOSTNAME || 'hapi-fhir'
+const HAPI_FHIR_PORT = process.env.HAPI_FHIR_PORT || 8080
 
 const downloadResources = (cbk) => {
   const COVID19_IG_URL = process.env.COVID19_IG_URL || 'https://openhie.github.io/covid-ig'
@@ -34,63 +37,56 @@ const downloadResources = (cbk) => {
   }).on('error', cbk)
 }
 
-const postToFHIRServer = (files) => {
-  const HAPI_FHIR_PATH =
-    process.env.HAPI_FHIR_PATH || '/fhir'
-  const HAPI_FHIR_HOSTNAME = process.env.HAPI_FHIR_HOSTNAME || 'hapi-fhir'
-  const HAPI_FHIR_PORT = process.env.HAPI_FHIR_PORT || 8080
+const postToFHIRServer = (file) => new Promise((resolve, reject) => {
+  const resourceName = file.split(/\-/)[0]
+  const data = fs.readFileSync(path.resolve(resourcesPath, file))
 
-  files.forEach(file => {
-    const resourceName = file.split(/\-/)[0]
-    const data = fs.readFileSync(path.resolve(resourcesPath, file))
-  
-    const options = {
-      protocol: process.env.COVID19_IG_PROTOCOL || 'http:',
-      host: HAPI_FHIR_HOSTNAME,
-      port: HAPI_FHIR_PORT,
-      path: `${HAPI_FHIR_PATH}/${resourceName}`,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(data),
-      }
+  const options = {
+    protocol: process.env.COVID19_IG_PROTOCOL || 'http:',
+    host: HAPI_FHIR_HOSTNAME,
+    port: HAPI_FHIR_PORT,
+    path: `${HAPI_FHIR_PATH}/${resourceName}`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(data),
     }
-  
-    const req = http.request(options, (res) => {
-      if (res.statusCode !== 201) {
-        let responseData = ''
-        res.on('data', (chunk) => {
-          responseData += chunk.toString()
-        })
-  
-        res.on('end', () => {
-          if (responseData) {
-            throw Error(`${resourceName} resource creation failed: ${responseData}`)
-          }
-        })
-  
-        res.on('error', (err) => {
-          throw Error(err)
-        })
-        return
-      } else {
-        console.log(`Successfully created ${resourceName} resource`)
-      }
-    })
-  
-    req.on('error', (error) => {
-      console.error('Failed to add resource: ', error)
-    })
-  
-    req.write(data)
-    req.end()
+  }
+
+  const req = http.request(options, (res) => {
+    if (res.statusCode !== 201) {
+      let responseData = ''
+      res.on('data', (chunk) => {
+        responseData += chunk.toString()
+      })
+
+      res.on('end', () => {
+        if (responseData) {
+          reject(`${resourceName} resource creation failed: ${responseData}`)
+        }
+      })
+
+      res.on('error', reject)
+      return
+    } else {
+      resolve(`Successfully created ${resourceName} resource`)
+    }
   })
-}
+
+  req.on('error', (error) => {
+    reject('Failed to add resource: ', error)
+  })
+
+  req.write(data)
+  req.end()
+})
 
 downloadResources((err, files) => {
   if (err) {
     console.error(err)
   } else {
-    postToFHIRServer(files)
+    Promise.all(files.map(postToFHIRServer))
+      .then(console.log)
+      .catch(console.error)
   }
 })
