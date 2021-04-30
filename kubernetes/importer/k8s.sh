c252d6cd15c320c964a32da3490bec9c09c39fb5
MattyJ007 <matthew.dickie@jembi.org>
2021-04-30 15:05:03 +0200

    Remove wait loop from K8s import
    This was put in to give DHIS2 time to start up - but the issue is not
    yet solved and this loop is infinite.
    Needs a better solution
    
    DSC19-35

--- kubernetes/importer/k8s.sh
+++ kubernetes/importer/k8s.sh
@@ -6,18 +6,6 @@ k8sImporterRootFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 if [ "$1" == "up" ]; then
     kubectl apply -k $k8sImporterRootFilePath
     
-    actions() {
-      dhis2PodName=$( kubectl get pods --selector=component=dhis-metadata-config-importer -o jsonpath='{.items[*].metadata.name}' )
-    }
-
-    actions #1st execution
-    while [ -z "$dhis2PodName" ]; do
-        echo "DHIS2 Importer pod not ready. Sleep 2"
-        sleep 2
-        actions
-        echo "Pod Name: $dhis2PodName"
-    done
-    
     kubectl cp "$k8sImporterRootFilePath/volume/dhis2/metadata.js" $dhis2PodName:/importer -c check-config-files-exist
     kubectl cp "$k8sImporterRootFilePath/volume/dhis2/metadata.json.gz" $dhis2PodName:/importer -c check-config-files-exist
 elif [ "$1" == "clean" ]; then
