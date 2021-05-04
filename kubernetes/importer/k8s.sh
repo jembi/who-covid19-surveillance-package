4f085dd1253ca7d15db8e97a5c5f838a12fe8466
brad <bradford.sawadye@jembi.org>
2021-05-04 14:06:35 +0200

    Add logic for adding the dhis metadata
    This adds the metadata for configuring the dhis instance
    
    DSC19-115

--- kubernetes/importer/k8s.sh
+++ kubernetes/importer/k8s.sh
@@ -4,10 +4,8 @@ k8sImporterRootFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 
 if [ "$1" == "up" ]; then
+    kubectl create configmap dhis2-metadata-configmap --from-file "$k8sImporterRootFilePath/volume/dhis2"
     kubectl apply -k $k8sImporterRootFilePath
-    
-    kubectl cp "$k8sImporterRootFilePath/volume/dhis2/metadata.js" $dhis2PodName:/importer -c check-config-files-exist
-    kubectl cp "$k8sImporterRootFilePath/volume/dhis2/metadata.json.gz" $dhis2PodName:/importer -c check-config-files-exist
 elif [ "$1" == "clean" ]; then
     kubectl delete -k $k8sImporterRootFilePath
     kubectl delete jobs,persistentvolumeclaim --all
