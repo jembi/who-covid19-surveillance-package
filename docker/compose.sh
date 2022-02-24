68181c3715966f70e219bb783e33bbe6a16b58af
MattyJ007 <matthew.dickie@jembi.org>
2022-02-24 09:59:31 +0200

    Clean up config importers after run
    These containers remain attached to the instant volume which prevents
    volume clean up. Clean up is needed before services can be updated on
    future runs

--- docker/compose.sh
+++ docker/compose.sh
@@ -10,12 +10,22 @@ if [ "$1" == "init" ]; then
     docker rm file-queue-helper
 
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml up -d
+
+    echo "Sleep 90 - Give importers time to complete"
+    sleep 90
+
+    # docker rm covid19-surveillance-openhim-config-importer covid19-surveillance-config-importer dhis2-tracker-populator-config-importer hapi-fhir-config-importer dhis2-config-importer
 elif [ "$1" == "up" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml up -d
 elif [ "$1" == "down" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml stop
 elif [ "$1" == "destroy" ]; then
     docker-compose -p instant -f "$composeFilePath"/docker-compose.yml -f "$composeFilePath"/docker-compose.dev.yml -f "$composeFilePath"/importer/docker-compose.config.yml down
+
+    echo "Sleep 10. Give containers time to shut down before deleting volume"
+    sleep 10
+
+    docker volume rm file-queue-config
 else
     echo "Valid options are: init, up, down, or destroy"
 fi
